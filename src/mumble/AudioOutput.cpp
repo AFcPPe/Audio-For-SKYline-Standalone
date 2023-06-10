fc35a187aad04dd8de25fa7447ada577ea8c995e
AFcPPe <1196986752@qq.com>
2023-06-10 17:15:03 +0800

    调整了冲麦声

--- src/mumble/AudioOutput.cpp
+++ src/mumble/AudioOutput.cpp
@@ -732,9 +732,9 @@ bool AudioOutput::mix(void *outbuff, unsigned int frameCount) {
 	}
 	//=============================声音截断=============================
 	//====================冲麦效果====================
-	if (qlMix.size() >= 2) {
-		float fFreq      = 50.0; // 嘟的频率
-		float fAmplitude = 1.5f; // 嘟的幅度
+	if (qlMix.size() >= 0) {
+		float fFreq      = 100.0; // 嘟的频率
+		float fAmplitude = 0.8f; // 嘟的幅度
 		float fPhase     = 0.0f; // 正弦波的相位
 		for (unsigned int i = 0; i < frameCount * iChannels; i += 1) {
 			float fSample = output[i]; // 获取采样值
